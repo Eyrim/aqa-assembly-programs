73349f519d38a7d00d42a9e32b307b006a66a05a
Eyrim <marfxedbot@gmail.com>
2022-06-06 17:35:44 +0100

    First commit and divide-with-remainder added

---
+++ divide-with-remainder.asm
@@ -0,0 +1,13 @@
+      INP R0,2 // Dividend
+      INP R1,2 // Divisor
+      MOV R2,#0
+DIVIDE:
+      CMP R0,R1
+      BLT END
+      SUB R0,R0,R1
+      ADD R2, R2, #1
+      B DIVIDE
+END:
+      OUT R2,4 //Ans
+      OUT R0,4 //Remainder
+      HALT
